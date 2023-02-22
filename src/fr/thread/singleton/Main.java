4a52f565d87e1f221e2285bcce35beb3e3c3d917
khaled SARRAJ <khaled.sarraj@capegemini.com>
2023-02-22 09:51:51 +0100

    Initial commit

---
+++ src/fr/thread/singleton/Main.java
@@ -0,0 +1,13 @@
+package fr.thread.singleton;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Début de projet");
+        //TODO FIRST COMMIT
+        // write your code here
+
+        System.out.println("Fin de projet");
+
+    }
+}
